b3c5a1944cb7f70f3aafc346fd1c2114838bfa39
OscarB <69217859+oscarTinkerer37@users.noreply.github.com>
2021-01-26 17:54:14 -0600

    Update listOfSquaresPalindromes.py
    A routine that produces a list of integers from 10 to 899, squares them and alerts the user when the original integer is a palindrome and when its square is a palindrome.

--- listOfSquaresPalindromes.py
+++ listOfSquaresPalindromes.py
@@ -1,10 +1,11 @@
-#lista de cuadrados para hallar palindromas
+#list of squared numbers for finding palindromes
+#Created on windows pc
 
-import time, winsound, pprint
+import time, pprint
 
-winsound.Beep(400, 500)
-#winsound.MB_ICONHAND #can't make it work...
-print('Se imprimirá una lista de números de dos dígitos o más y sus cuadrados.\nEste programa indicará si alguna base no-palindrómica produce un cuadrado palindrómico.')
+#A list of numbers with lengths of two or more digits (10-899 default) along with their squared values will be printed out.
+#The program will alert the user when a non-palindromic base produces a palindromic square.
+      
 print()
 time.sleep(0.2)
 
@@ -12,7 +13,7 @@ bplusSquare = []
 onlySquare = []
 onlyBase = []
 
-for b in range(10, 900):
+for b in range(10, 900): #List of numbers
     cuadr = b**2
     base = list(str(b))
     base2 = base.copy()
@@ -26,36 +27,20 @@ for b in range(10, 900):
     time.sleep(0.1)
 
     if base2 == base and sq == cuadr2:
-        print('Base %s y cuadrado %s palindrómicos' %(b, cuadr))
-        #print()
+        print('Base %s and squared %s are palindromes' %(b, cuadr))
         bplusSquare.append(base)
         bplusSquare.append(sq)
-        winsound.Beep(320, 300) #Beep (Hertz, milisegundos)
         time.sleep(0.1)
         
     if base2 != base and sq == cuadr2:
-        print('Base %s no-palindrómica... Cuadrado %s Palindrómico' %(b, cuadr))
-        #print()
+        print('Base %s non-palindrome... Square %s is a palindrome' %(b, cuadr))
         onlySquare.append(base2)
         onlySquare.append(sq)
-        winsound.Beep(750, 350) #Beep (Hertz, milisegundos)
-        winsound.Beep(720, 310)
-        winsound.Beep(650, 320)
-        #winsound.MessageBeep(5) #-1 es el predeterminado
         time.sleep(0.1)
         
     if base2 == base and sq != cuadr2:
-        print('Base %s palindrómica... cuadrado %s no-palindrómico.' %(b, cuadr))
-        #print()
+        print('Base %s is a palindrome... square %s non-palindrome.' %(b, cuadr))
         onlyBase.append(base2)
         onlyBase.append(cuadr2)
-        winsound.Beep(300, 250) #Beep (Hertz, milisegundos)
         time.sleep(0.05)
 
-#polish this one
-#pprint not usable with list type
-#print('Base+Cuadrado: ', bplusSquare)
-#print()
-#print('Solo cuadrado: ', onlySquare)
-#print()
-#print('Solo base: ', onlyBase)
